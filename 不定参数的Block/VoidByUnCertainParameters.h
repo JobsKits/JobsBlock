994650972c4cca36f3afb82331cf9647be0c0c35
295060456@qq.com <lg295060456@gmail.com>
2026-01-15 13:57:02 +0700

    no message

--- 不定参数的Block/VoidByUnCertainParameters.h
+++ 不定参数的Block/VoidByUnCertainParameters.h
@@ -7,8 +7,8 @@
 
 #ifndef VoidByUnCertainParameters_h
 #define VoidByUnCertainParameters_h
-
-/* 第一个参数写：有多少个实际参数 */
-typedef void(^jobsByUnCertainParamBlock)(id firstArg,...);
+/// 第一个参数写：有多少个实际参数
+typedef void(^jobsDynArgsBlock)(NSInteger argCount,id _Nullable firstArg,...);
+typedef void(^jobsByStringAndNSUIntegerBlock)(__kindof NSString *_Nullable data1,NSUInteger data2);
 
 #endif /* VoidByUnCertainParameters_h */
